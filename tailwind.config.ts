00f517b56e7a8d50b4af4f424b1a8337e4801f28
emnesty <legostr@gmail.com>
2024-03-11 08:10:38 -0300

    First Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
+      colors: {
+        herobackground: "#0C111D",
+        texthero: "#929FB1",
+        texthero2: "#B3B7BD",
+        white: "#ffffff",
+        black: "#000000",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero-background": "url('/images/Background pattern2.svg')",
       },
     },
   },
