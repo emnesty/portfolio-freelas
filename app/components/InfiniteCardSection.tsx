605d7c5fba89fd3f14968f50c97f183aefbba8cb
Luciano Silva <luciano.clecio@ambevtech.com.br>
2024-03-12 15:28:53 -0300

    Ajustes

--- app/components/InfiniteCardSection.tsx
+++ app/components/InfiniteCardSection.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "./ui/infiniteMovingCards";
 
-export default function InfiniteMovingCardsDemo() {
+export default function InfiniteMovingCardsSection() {
   return (
     <div className="h-[40rem] flex flex-col antialiased bg-white dark:bg-herobackground dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
-import { InfiniteMovingCards } from "./components/ui/infiniteMovingCards";
-import { InfiniteMovingCardsDemo } from "./components/InfiniteCardSection";
+import InfiniteMovingCardsSection from "./components/InfiniteCardSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <InfiniteMovingCardsDemo />
+      <InfiniteMovingCardsSection />
     </>
   );
 }
