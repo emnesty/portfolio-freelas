f21aa3b5c8374bfc57c9b8d59d675e120d23ec84
Luciano Silva <luciano.clecio@ambevtech.com.br>
2024-03-12 16:25:10 -0300

    Ajustes

--- app/components/InfiniteMovingCardsSection.tsx
+++ app/components/InfiniteMovingCardsSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { InfiniteMovingCards } from "./ui/infiniteMovingCards";
+import { InfiniteMovingCards } from "../components/ui/InfiniteMovingCards";
 
 export default function InfiniteMovingCardsSection() {
   return (
