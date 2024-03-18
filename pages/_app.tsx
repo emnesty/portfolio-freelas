dc6719082a8938d5f816d9309fc358950248b38a
Luciano Silva <luciano.clecio@ambevtech.com.br>
2024-03-18 15:04:39 -0300

    Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
 import "../app/globals.css"; // O caminho correto para o seu arquivo CSS do Tailwind
 import type { AppProps } from "next/app";
 import AOSInit from "../app/components/AOSInit";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Luciano Silva UX/UI Designer - Front-End Developer",
+  description:
+    "Olá, sou Luciano Silva, um UX/UI Designer e Desenvolvedor Frontend com experiência em grandes empresas de tecnologia. Neste portfólio, você poderá explorar meus trabalhos criativos, recomendações e habilidades adquiridas ao longo da minha jornada profissional. Sinta-se à vontade para se conectar comigo no LinkedIn e explorar meus projetos no Github para ter uma visão mais aprofundada do meu expertise técnico e criativo.",
+  metadataBase: new URL("https://lucianosilva.cc/"),
+  applicationName: "Luciano Silva Portfolio",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Luciano Silva",
+    "UX/UI Designer",
+    "Product Designer",
+    "Front-End Developer",
+    "Experiência em Tecnologia",
+    "Portfólio Criativo",
+    "Recomendações Profissionais",
+    "Habilidades de Design",
+    "Desenvolvimento Web",
+    "LinkedIn Networking",
+    "Projetos GitHub",
+    "Expertise Técnico",
+    "Design Criativo",
+    "Indústria de Tecnologia",
+    "Desenvolvedor Experiente",
+    "Inovação Digital",
+  ],
+  openGraph: {
+    type: "website",
+    url: "https://lucianosilva.cc/",
+    title: "Luciano Silva UX/UI Designer - Front-End Developer",
+    description:
+      "Olá, sou Luciano Silva, um UX/UI Designer e Desenvolvedor Frontend com experiência em grandes empresas de tecnologia. Neste portfólio, você poderá explorar meus trabalhos criativos, recomendações e habilidades adquiridas ao longo da minha jornada profissional. Sinta-se à vontade para se conectar comigo no LinkedIn e explorar meus projetos no Github para ter uma visão mais aprofundada do meu expertise técnico e criativo.",
+    siteName: "Luciano Silva UX/UI Designer - Front-End Developer",
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
