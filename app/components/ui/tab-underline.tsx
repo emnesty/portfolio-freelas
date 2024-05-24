72d135934a2446b46b474cc02963a84608676585
emnesty <legostr@gmail.com>
2024-05-24 09:48:38 -0300

    Ajustes

--- app/components/ui/tab-underline.tsx
+++ app/components/ui/tab-underline.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 const tabs = [
   { name: "Todos", href: "#", current: true },
   { name: "Design (UI)", href: "#", current: false },
@@ -26,7 +28,7 @@ export default function BarUnderline({
         <select
           id="tabs"
           name="tabs"
-          className="block w-full rounded-md border-gray-300 focus:border-[#0070f3] focus:ring-[#0070f3]"
+          className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
           value={selectedTab}
           onChange={(e) => setSelectedTab(e.target.value)}
         >
@@ -38,32 +40,20 @@ export default function BarUnderline({
         </select>
       </div>
       <div className="hidden sm:block">
-        <nav
-          className="isolate flex divide-x divide-gray-200 rounded-lg shadow"
-          aria-label="Tabs"
-        >
-          {tabs.map((tab, tabIdx) => (
+        <nav className="flex space-x-4 justify-center" aria-label="Tabs">
+          {tabs.map((tab) => (
             <button
               key={tab.name}
               onClick={() => setSelectedTab(tab.name)}
               className={classNames(
                 tab.name === selectedTab
-                  ? "text-gray-900"
-                  : "text-gray-500 hover:text-gray-700",
-                tabIdx === 0 ? "rounded-l-lg" : "",
-                tabIdx === tabs.length - 1 ? "rounded-r-lg" : "",
-                "group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
+                  ? "bg-gray-200 text-gray-800"
+                  : "text-gray-600 hover:text-gray-800",
+                "rounded-md px-3 py-2 text-sm font-medium"
               )}
               aria-current={tab.name === selectedTab ? "page" : undefined}
             >
-              <span>{tab.name}</span>
-              <span
-                aria-hidden="true"
-                className={classNames(
-                  tab.name === selectedTab ? "bg-indigo-500" : "bg-transparent",
-                  "absolute inset-x-0 bottom-0 h-0.5"
-                )}
-              />
+              {tab.name}
             </button>
           ))}
         </nav>
