30bfa14c329b598c3c527feafa37a63b502e5550
emnesty <legostr@gmail.com>
2024-06-07 22:38:51 -0300

    Ajustes

--- app/components/ProjectsCarrousel.tsx
+++ app/components/ProjectsCarrousel.tsx
@@ -11,7 +11,7 @@ export function ProjectsCarrousel() {
   useEffect(() => {
     gsap.fromTo(
       "#animated-image",
-      { x: "70%" },
+      { x: "50%" },
       {
         x: "0%",
         duration: 3.5,
@@ -47,7 +47,7 @@ export function ProjectsCarrousel() {
               id="animated-image"
               loading="lazy"
               src="/images/app.svg"
-              className="h-full sm:max-w-none max-sm:max-w-[81rem]"
+              className="h-full max-w-none"
             />
           </div>
         </div>
