23279bb288df94231630005ac8cd3056be2aad0c
Luciano Silva <legostr@gmail.com>
2024-06-17 19:16:36 -0300

    Revert "Ajustes"
    This reverts commit c537a4686f71506da44fdf7b8a6e385847e2e72a.

--- app/components/ProjectsCarrousel.tsx
+++ app/components/ProjectsCarrousel.tsx
@@ -11,9 +11,9 @@ export function ProjectsCarrousel() {
   useEffect(() => {
     gsap.fromTo(
       "#animated-image",
-      { x: "70%" },
+      { x: "50%" },
       {
-        x: "-30%",
+        x: "0%",
         duration: 3.5,
         ease: "circ.out",
         scrollTrigger: {
