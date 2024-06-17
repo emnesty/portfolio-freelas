d5aaabb86f7cf1f7574ee6cbff296a87bc83069f
Luciano Silva <legostr@gmail.com>
2024-06-17 19:05:53 -0300

    Revert "Ajustes"
    This reverts commit 275c42c10eb9f719e7a48d740a00ad4034d59e7e.

--- app/data/projetosData.ts
+++ app/data/projetosData.ts
@@ -35,11 +35,7 @@ export const projetosData = [
     description:
       "Projeto de uma Pokedex feita com CSS, JavaScript e HTML, consumindo API para listagem dos Pokemons dinamicamente",
     imgsrc: "/images/projetos/pokemonlp.webp",
-    images: [
-      "/images/projetos/pokemon/Pokemon 1.png",
-      "/images/projetos/pokemon/Pokemon 2.png",
-      "/images/projetos/pokemon/Pokemon 3.png",
-    ],
+    images: [],
     urlsite: "https://projeto-pokemon-r1vm.vercel.app/",
     urlgithub: "https://github.com/emnesty/projeto-pokemon",
     descriptiondetails: "",
