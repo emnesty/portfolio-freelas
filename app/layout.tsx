e919e1f4eae906d1f188ae3a9557d20cddaf4050
Luciano Clécio da Silva <legostr@gmail.com>
2024-06-26 11:40:36 -0300

    Ajustes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { SmoothScroll } from "./components/SmoothScroll";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const sora = Sora({
   weight: "400",
@@ -65,6 +66,7 @@ export default function RootLayout({
       <body className={sora.className}>
         <SmoothScroll>{children}</SmoothScroll>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
